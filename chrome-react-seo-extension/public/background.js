b0733b473e52fef3504a2c1b06c146027095b79d
poudyalankit <poudyalankit@gmail.com>
2023-03-26 05:28:59 -0400

    added background.js

---
+++ chrome-react-seo-extension/public/background.js
@@ -0,0 +1,50 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request !== 'getCookies') return false;
+  
+    const noCookies = { cookies: null };
+    chrome.tabs.query({
+      currentWindow: true,
+      active: true
+    }, (tabs) => {
+      if (tabs.length === 0) {
+        sendResponse(noCookies);
+        return;
+      }
+      const target = tabs[0];
+  
+      const param = {
+        url: target.url
+      };
+      if (!/^http/.test(param.url)) {
+        sendResponse(noCookies);
+        return;
+      }
+  
+      if (target.cookieStoreId) {
+        param.storeId = target.cookieStoreId;
+      }
+  
+      chrome.cookies.getAll(param, (cookies) => {
+        sendResponse({
+          domain: new URL(param.url).hostname,
+          cookies: cookies.map((c) => {
+            const result = {
+              name: c.name,
+              value: c.value,
+              domain: c.domain,
+              path: c.path,
+              expires: c.expirationDate || -1,
+              httpOnly: c.httpOnly,
+              secure: c.secure
+            };
+            if (['lax', 'strict'].includes(c.sameSite)) {
+              result.sameSite = c.sameSite.replace(/^./, (p) => p.toUpperCase());
+            }
+          return cookies;
+          })
+        });
+      });
+    });
+  
+    return true;
+  });
