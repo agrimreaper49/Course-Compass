b8e17d3ec4a2748712e794d7a35241f6805b862b
jammel-yeboah <93840983+jammel-yeboah@users.noreply.github.com>
2023-03-26 03:27:20 -0400

    added logo

--- chrome-react-seo-extension/src/App.tsx
+++ chrome-react-seo-extension/src/App.tsx
@@ -1,30 +1,17 @@
 //import React from 'react';
-import logo from './logo.svg';
+import logo from './saber.png';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
 
 
 function App() {
   const [myString, setMyString] = useState<string>('');
-  // useEffect(() => {
-  //   console.log("Hello")
-  //   axios.get('http://127.0.0.1:5000/string')
-  //     .then((response: { data: { string: string } }) => {
-  //       setMyString(response.data.string);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  //     console.log(myString)
-  // }, []);
   useEffect(() => {
     fetch("http://127.0.0.1:5000/string")
       .then((res) => res.json())
       .then((res) => {
         setMyString(res.string);
-        console.log("!!!!", myString )
+        console.log(myString )
       });
   }, []);
 
@@ -36,20 +23,9 @@ function App() {
         <p>
           {myString}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-
-
 }
-
 
 export default App;
