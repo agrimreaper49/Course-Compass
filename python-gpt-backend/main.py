a4ea7a262b0727369ceb5c284b7742a39008c7d8
jammel-yeboah <93840983+jammel-yeboah@users.noreply.github.com>
2023-03-25 21:16:36 -0400

    Update main.py

--- python-gpt-backend/main.py
+++ python-gpt-backend/main.py
@@ -1,15 +1,17 @@
 import openai
 
-major = ""
-courses = ""
+major = "Computer Science"
+courses = "CS 1110, DSA 2100, CS 2130"
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
             {"role": "system", "content": "You are a chatbot"},
-            {"role": "user", "content": "I'm a {major} major and I've taken these courses: {courses}. What classes do you recomend I take next"},
+            {"role": "user", "content": f"I'm a {major} major at UVA and I've taken these courses: {courses}. Recomend classes I should take next"},
         ]
 )
+
+
 result = ''
 for choice in response.choices:
     result += choice.message.content
